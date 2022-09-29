68d52779cb3ab287bca6e85e5c56046c2e45aee5
Deepak Agrawal <deepakagrawal@Deepaks-MacBook-Pro.local>
2022-09-29 06:36:37 +0530

    Added ELK APM

--- autoapp.py
+++ autoapp.py
@@ -4,7 +4,15 @@ from flask.helpers import get_debug_flag
 
 from conduit.app import create_app
 from conduit.settings import DevConfig, ProdConfig
+from elasticapm.contrib.flask import ElasticAPM
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
+
+app.config['ELASTIC_APM'] = {
+    'SERVICE_NAME': 'FlaskApp',
+    'SERVER_URL': 'http://10.1.1.180:8200'
+}
+
+apm = ElasticAPM(app)
